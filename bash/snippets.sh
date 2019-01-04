c902ef293cf6bdb39470d2cc54c1652381d7e9cd
Stepan Sindelar <me@stevesindelar.cz>
2019-01-04 12:06:45 +0100

    Update snippets.sh

--- bash/snippets.sh
+++ bash/snippets.sh
@@ -17,13 +17,13 @@ function get-auto-title() {
 }
 
 function set-auto-title(){
-  if [ -z "$var" ]; then
+  if [ -z "$STEVE_CUSTOM_TITLE" ]; then
     echo -ne "\033]0;$(get-auto-title)\007"
   fi
 }
 
 function preexec_invoke_exec () {  
-  if [ -z "$var" ]; then
+  if [ -z "$STEVE_CUSTOM_TITLE" ]; then
     [ -n "$COMP_LINE" ] && return  # do nothing if completing
     [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
     local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
